7263ba81d7c2dfa58dbcda5fa4c29ef6c50a62fd
Setia Anggraeni <tia.anggraeni2@gmail.com>
2018-05-29 16:00:45 +0700

    Check All Vowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,29 @@
+function checkAllVowels(word){
+    var count=0
+    for(let i=0; i<word.length; i++){
+        if(word[i] === "A" || word[i] === "a"){
+            count+=1
+        }
+        else if(word[i] === "I" || word[i] === "i"){
+            count+=1
+        }
+        else if(word[i] === "U" || word[i] === "u"){
+            count+=1
+        }
+        else if(word[i] === "E" || word[i] === "e"){
+            count+=1
+        }
+        else if(word[i] === "O" || word[i] === "o"){
+            count+=1
+        }
+    }
+    if(count === word.length){
+        return true
+    }
+    return false
+}
 
+console.log(checkAllVowels("aiueo"))
+console.log(checkAllVowels("iau"))
+console.log(checkAllVowels("apa"))
+console.log(checkAllVowels("kakak"))
