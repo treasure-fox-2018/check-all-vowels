e2b9eca2e125850fd262d11c60518536137a24b1
andrewsasmita <its.me.andrew.sasmita@gmail.com>
2018-05-29 23:28:36 +0700

    ok

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,29 @@
+// checkAllVowels
+
+function checkAllVowels(input) {
+
+    var vowel='aiueo'
+    var str=input.toLowerCase()
+    var counter=0
+
+    for(var x=0; x<str.length; x++) {
+        for(var y=0; y<vowel.length; y++) {
+            if(str[x]===vowel[y]) {
+                counter += 1
+            }
+        }
+    }
+
+    if(counter===str.length) {
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(checkAllVowels('abcde')) //FALSE
+console.log(checkAllVowels('AiUOe')) //TRUE
+console.log(checkAllVowels('AEObfg')) //FALSE
+console.log(checkAllVowels('aeiO')) //TRUE
+
 
