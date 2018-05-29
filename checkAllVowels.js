41b577c43a1868cb8e7b436e9b27bf0328d6bb4c
mario <panggihmario@gmail.com>
2018-05-29 13:47:09 +0700

    add-vowel-check

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,21 @@
-
+function checkVowels(str){
+    let vocal ='aiueo'
+    let kecil = str.toLowerCase()
+    let count =0
+    for(let i=0;i<kecil.length;i++){
+        for(let j=0;j<vocal.length;j++){
+            if(kecil[i] == vocal[j]){
+                count++
+            }
+        }
+    }
+    if(count == kecil.length){
+        return true
+    }
+        return false
+}
+console.log(checkVowels('aio'))
+console.log(checkVowels('abcdeiii'))
+console.log(checkVowels('AoaIeOo'))
+console.log(checkVowels('AIuTP'))
+console.log(checkVowels('AOIUUuaoi'))
