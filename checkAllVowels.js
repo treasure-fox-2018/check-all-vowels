a394474c48b44e18971642941c19d7f8d10cdc32
fadhilhaka <fadhilhanri@gmail.com>
2018-05-29 14:08:21 +0700

    check all vowels fadhil hanri kamarz

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,21 @@
 
+function checkVowel(str) {
+  var vowel = 'aiueoAIUEO'
+  var count = 0
+  for (var a = 0; a < str.length; a++) {
+    for (var b = 0; b < vowel.length; b++) {
+      if (str[a] === vowel[b]) {
+        count++
+      }
+    }
+  }
+  if (str.length === count) {
+    return true
+  } else {
+    return false
+  }
+}
+
+console.log(checkVowel('abcdefghijklmnopqrstuvwxyz'))
+console.log(checkVowel('aiueo'))
+console.log(checkVowel('aoIEoa'))
