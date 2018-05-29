e9ff5f8027ec373d810ae0a0281b8d691ae1e05c
wisnugautama <ngurahwisnugautama@gmail.com>
2018-05-29 14:41:50 +0700

    exercise check all vowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,23 @@
+function checkVocals(string) {
+  var hurufVocals = ['a','i','u','e','o','A','I','U','E','O'];
+  var newStr = '';
 
+  for (var i = 0; i < string.length; i++) {
+    for (var j = 0; j < hurufVocals.length; j++) {
+      if (string[i] === hurufVocals[j]) {
+        newStr += string[i];
+      }
+    }
+  }
+
+  if (newStr.length === string.length) {
+    return true;
+  }else {
+    return false;
+  }
+}
+console.log(checkVocals('semprul'));
+console.log(checkVocals('wuhu'));
+console.log(checkVocals('asdrtgh'));
+console.log(checkVocals('aiueo'));
+console.log(checkVocals('aiIUEA'));
