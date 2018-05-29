4576b9e8d157e856e8817042ef33f63c91422d36
HelmiYogantara <helmiyogantara@gmail.com>
2018-05-29 22:21:07 +0700

    exercise done !

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,21 @@
+function cekVocals(str){
+    var vocals = ['a','i','u','e','o','A','I','U','E','O']
+    var vocalString = []
+    for(var i = 0; i < str.length; i++){
+        for(var j = 0; j < vocals.length; j++){
+            if(str[i] === vocals[j]){
+                vocalString.push(str[i])
+            }
+        }
+    }
+
+    if(vocalString.length === str.length){
+        return true
+    }else{
+        return false
+    }
+}
+
+console.log(cekVocals('aaaa'))
+console.log(cekVocals('aouhb'));
 
