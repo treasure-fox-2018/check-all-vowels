b1c093219197c94a1869187dd0d282774181a641
handipriyono <handimacbook@gmail.com>
2018-05-29 10:43:29 +0700

    add vokal check

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,26 @@
+function checkVokal(kata) {
 
+  let vokal = 'aiueo';
+
+  for (let i = 0; i < kata.length; i++) {
+    let cek = false
+    for (let j = 0; j < vokal.length; j++) {
+      if (kata[i].toLowerCase() === vokal[j]) {
+        cek = true
+      }
+    }
+
+
+    if (!cek) {
+      return false
+    }
+  }
+
+
+
+  return true
+}
+
+console.log(checkVokal('aiuIaUui'));
+console.log(checkVokal('auiuhja'));
+console.log(checkVokal('aiuoauuiaiaouuaau'));
