2020cc971742a209cfac38cc026a54ec32344bf5
Your Name <fahrisyany@gmail.com>
2018-05-29 20:24:57 +0700

    challange done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,36 @@
 
+
+
+function vowelCheck(str) {
+    var vowels = 'aiueo'
+    var vowelCounter = 0
+
+
+    for (i = 0; i < vowels.length; i++) {
+        for (j = 0; j < str.length; j++) {
+            if (vowels[i] === str[j]) {
+                vowelCounter++
+            }
+            if (vowelCounter === str.length) {
+                return true
+            }
+        }
+    }
+    return false
+ 
+}
+console.log(vowelCheck('aeeeooo'))
+console.log(vowelCheck('kriStya'))
+console.log(vowelCheck('gogi92gagagagigo'))
+console.log(vowelCheck('uaeoua'))
+
+
+
+
+
+
+
+
+
+
+
