9a913910546d7e763d6207fcda32ab782e910a80
Odelia <odeliasusanto@gmail.com>
2018-05-29 13:00:01 +0700

    check vowels exercise done

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,22 @@
+function checkVowels(str) {
+    var vowels = 'aeiou';
 
+    for (var i = 0; i < str.length; i++) {
+        var isVowels = false;
+        for (var j = 0; j < vowels.length; j++) {
+            if (str[i] === vowels[j]) {
+                isVowels = true;
+            }
+        }
+        if (!isVowels) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(checkVowels('aeiou'));
+console.log(checkVowels('aaiioouu'));
+console.log(checkVowels('abcde'));
+console.log(checkVowels('zyx'));
+console.log(checkVowels('abcdefghimnotuv'));
