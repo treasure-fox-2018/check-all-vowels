8330fa1d75fb2c328f807a27e39a5fefb6d0a593
Tushar <tusharbedi7373@hotmail.com>
2018-05-29 17:03:51 +0700

    completed check vowels exercise

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,23 @@
 
+function checkAllVowels(string) {
+  const vowels = 'aeiou';
+
+  for (let i = 0; i < string.length; i++) {
+    let checker = false;
+    for (let j = 0; j < vowels.length; j++) {
+      if (string[i] === vowels[j]) {
+        checker = true;
+        break;
+      }
+    }
+    if (!checker) {
+      return false
+    }
+    // console.log(checker)
+  }
+  return true;
+
+}
+
+console.log(checkAllVowels('uoiauai')); // true
+console.log(checkAllVowels('hacktiv8')); // false
