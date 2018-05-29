f12d6d3df2f43b693fdba041e257bf9ecfb4e647
Wahyudi Setiaji <wahyudisetiaji@gmail.com>
2018-05-29 23:31:24 +0700

    ADD check all vowels

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,1 +1,23 @@
+function checkAllVowels (input){
+  let hurufVocal = "aiueo";
+  let countVocal = 0;
+  for(var i = 0; i < input.length; i++){
+    for(var j = 0; j < hurufVocal.length; j++){
+      if(input[i].toLowerCase() === hurufVocal[j]) {
+        // return true
+        countVocal++
+      }
+    }
+  }
+  if(countVocal !== 0){
+    return true
+  }else{
+    return false
+  }
+  // return false;
+}
 
+console.log(checkAllVowels("Wahyudi"));
+console.log(checkAllVowels("Brngkt"));
+console.log(checkAllVowels("Iaaaa"));
+console.log(checkAllVowels("Setiaji"));
