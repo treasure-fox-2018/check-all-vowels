bf3fa9403e14524d47040f37f5982f3029172c84
Your Name <fahrisyany@gmail.com>
2018-05-30 21:25:08 +0700

    challenge complete

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -4,25 +4,32 @@
 function vowelCheck(str) {
     var vowels = 'aiueo'
     var vowelCounter = 0
+    var string=''
 
+    for (j = 0; j < str.length; j++) {
+        if(str[j]!==' '){
+
+            string+=str[j]
+        }
+    }
 
     for (i = 0; i < vowels.length; i++) {
-        for (j = 0; j < str.length; j++) {
-            if (vowels[i] === str[j]) {
+        for (j = 0; j < string.length; j++) {
+            if (vowels[i] === string[j]) {
                 vowelCounter++
             }
-            if (vowelCounter === str.length) {
+            if (vowelCounter === string.length) {
                 return true
             }
         }
     }
     return false
- 
+
 }
-console.log(vowelCheck('aeeeooo'))
+console.log(vowelCheck('ae ee ooo'))
 console.log(vowelCheck('kriStya'))
 console.log(vowelCheck('gogi92gagagagigo'))
-console.log(vowelCheck('uaeoua'))
+console.log(vowelCheck('uaeo ua'))
 
 
 
