9ead76578821625635d96af57f6f4bab281543d6
ronifajrian <ronifajrian@gmail.com>
2018-05-30 11:54:58 +0700

    fix code

--- checkAllVowels.js
+++ checkAllVowels.js
@@ -1,36 +1,22 @@
-function checkAllVowels(jmlRow, jmlCol) {
-    var huruf = 'aaaaiiiiuuueeeoookdfkdfjep'
-    var arr = []
-    for (let i = 0; i < jmlRow; i++) {
-        var isi = []
-        for (let j = 0; j < jmlCol; j++) {
-            var num = Math.floor(Math.random() * 26)
-            isi.push(huruf[num])
-       }
-       arr.push(isi)
-    }
+function isVowel(kata) {
+    var huruf = 'aiueo'
 
-    var indexVowel = []
-    for (let i = 0; i < jmlRow; i++) {
-        var arrIsi = []
-        for (let j = 0; j < jmlCol; j++) {
-            if (arr[i][j] == 'a' || arr[i][j] == 'i' || arr[i][j] == 'u' || arr[i][j] == 'e' || arr[i][j] == 'o') {
-                arrIsi.push(j)
+    for (let i = 0; i < kata.length; i++) {
+        var check = false
+        for (let j = 0; j < huruf.length; j++) {
+            if (kata[i] == huruf[j]) {
+                check |= true
             }
         }
-        indexVowel.push(arrIsi)
-    }
-
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr[i].length; j++) {
-            
+        check &= true
+        if (!check) {
+            return false
         }
     }
-
-    console.log(arr);
+    return true
     
-    return indexVowel
+    
 }
-
-console.log(checkAllVowels(6,4));
-
+console.log(isVowel('test'));
+console.log(isVowel('aoaaaaaaoui'));
+     
